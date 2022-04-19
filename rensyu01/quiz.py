bf66f3180d2955c1c54b83d6d2989716dcb73e5c
c0b21070 <c0b21070c0@edu.teu.ac.jp>
2022-04-19 18:02:47 +0900

    修正#4

--- rensyu01/quiz.py
+++ rensyu01/quiz.py
@@ -5,7 +5,7 @@ def shutudai():
     print("じゃじゃん！！",qa["q"])
     return qa["a"]
 
-def kaito(a_list):
+def kai(a_list):
     kai = input("答えを入力して下さい：")
     print("入力した答えは：", kai)
     if kai in a_list:
@@ -17,5 +17,5 @@ def kaito(a_list):
 if __name__ == "__main__":
     qa_list =[{"q" : "サザエさんの旦那の名前は？","a": ["ますお","マスオ"]},{"q" : "カツオの妹の名前は？", "a": ["わかめ","ワカメ"]},{"q" : "タラオはカツオから見てどんな関係？", "a": ["甥","おい","甥っ子",""]}]
     a_list = shutudai()
-    kaito(a_list)
+    kai(a_list)
 
