54e71b1297c84bbb608017117e79a059410a5848
c0b21070 <85671233+c0b21070@users.noreply.github.com>
2022-04-26 19:03:41 +0900

    Update calc.py

--- kadai02/calc.py
+++ kadai02/calc.py
@@ -1,73 +1,92 @@
 import tkinter as tk
-from  tkinter import messagebox as tkm
-import tkinter
+import tkinter.messagebox as tkm
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.geometry('300x450')
+    button_list = []
+    r=5
+    c=3
+    def button_click(event):
+        btn = event.widget
+        entry.insert(tk.END, btn['text'])
+    def evel(event):
+        lit = entry.get()
+        ans = 0
+        if '+' in lit:
+            nums = lit.split('+')
+            for i in nums:
+                ans += int(i)
 
-calc =  tk.Tk()
-calc.geometry("300x450")
-calc.title = ("電卓")
+        if '-' in lit:
+            nums = lit.split('-')
+            for i in nums:
+                ans -= int(i)
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, str(ans))
 
-entry = tk.Entry(justify = "right", width = 10, font = ("Times New Roman",40))
-entry.grid(row = 0, columnspan = 10)
 
-def button_click(event):
-    btn = event.widget
-    #button["bg"]="gray"
-    txt = btn["text"]
-    if txt=="−":
-        entry.insert(tk.END,"-")
-    elif txt=="×":
-        entry.insert(tk.END,"*")
-    else:
-        entry.insert(tk.END, txt)
-    #tkm.showinfo(txt, f"[{txt}]ボタンがクリックされました")
+    def b_clear(event):
+        new = entry.get()
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, new[:-1])
 
-def equal(event):
-    siki = entry.get()
-    entry.delete(0, tk.END)
-    kotae = eval(siki)
-    entry.insert(tk.END, kotae)
+    def b_aclear(event):
+        entry.delete(0, tk.END)   
+
+    entry = tk.Entry(width=10,
+                    justify='right',
+                    font=('Times New Roman', 40)
+                    )
+    entry.grid(column=0, row=0, columnspan=4, rowspan=1)
+    for i in range(10):
+        button = tk.Button(root, text=i,
+                            font=('Times New Roman', 30)
+                            )
+        
+        if r == 5:
+            c = 0
+            button.grid(row = r, column=c, padx=0, pady=0)
+            button_list.append(button)
+            r -= 1
+            c = 2
+            continue
+        if c == 0:
+            button.grid(row = r, column=c, padx=0, pady=0)
+            button_list.append(button)
+            r -= 1
+            c = 2
+            continue
+        
+        button.grid(row=r, column=c, padx=0, pady=0)
+        button_list.append(button)
+        c -= 1
     
-def delete(event):
-    entry.delete(0, tk.END)
-   
-fonts = ("Times New Roman", 20)
-fonts2=("Times New Roman", 10)
-x = 0
-list = [[7,1],[6,2],[6,1],[6,0],[5,2],[5,1],[5,0],[4,2],[4,1],[4,0]]
-for i in list:
-    button = tk.Button(text= x,font=fonts,width = 4, height = 1)
-    button.bind("<1>", button_click)
-    button.grid(row = i[0],column = i[1],padx=2,pady = 4)
-    x += 1
-
+    for i in button_list:
+        i.bind('<1>', button_click)
     
-buttonequal = tk.Button(text='=',font=fonts,width = 4, height = 1,background = "gray")
-buttonequal.bind("<1>", equal)
-buttonequal.grid(row = 7,column = 4,padx=2,pady = 4)
+    button_plus = tk.Button(root, text='+',
+                            font=('Times New Roman', 30)
+                            )
+    button_plus.bind('<1>', button_click)
+    
+    button_plus.grid(row=5, column=1, padx=0, pady=0)
+    button_equal = tk.Button(root, text='=',
+                            font=('Times New Roman', 30)
+                            )
+    button_equal.bind('<1>', evel)
+    button_equal.grid(row=5, column=2)
 
-buttonDel = tk.Button(text='AC',font=fonts,width = 4, height = 1,background = "gray")
-buttonDel.bind("<1>", delete)
-buttonDel.grid(row = 2,column = 4,padx=2,pady = 4)
+    button_clear = tk.Button(root, text='C',
+                            font=('Times New Roman', 30)
+                            )
+    button_clear.bind('<1>', b_clear)
+    button_clear.grid(row=5, column=3)
 
+    button_aclear = tk.Button(root, text='AC',
+                            font=('Times New Roman', 30)
+                            )
+    button_aclear.bind('<1>', b_aclear)
+    button_aclear.grid(row=4, column=3)
 
-list1 = [["+"],['−'],['×'],['/']]
-y = 3
-for i in list1:
-    button = tk.Button(text= i,font=fonts,width = 4, height = 1,background = "gray")
-    button.bind("<1>", button_click)
-    button.grid(row = y,column = 4,padx=2,pady = 4)
-    y += 1
-
-list1 = [['('],[')'],["C"]]
-y = 0
-for i in list1:
-    button = tk.Button(text= i,font=fonts,width = 4, height = 1,background = "gray")
-    if y==2:
-        button.bind("<1>", button_delete2)
-    else:
-        button.bind("<1>", button_click)
-    button.grid(row = 3,column = y,padx=2,pady = 4)
-    y += 1
-def button_delete2(event):
-    entry.delete(len(entry.get())-1,tk.END)
-calc.mainloop()
+    root.mainloop() 
+0 comments on commit 85b78e4
