3475989270ac05c7dc40b41e16c01bf5af855496
Kaito Fujimagari <85667950+c0b21138@users.noreply.github.com>
2022-04-26 18:26:22 +0900

    Update calc.py

--- kadai02/calc.py
+++ kadai02/calc.py
@@ -1,79 +1,73 @@
-import math
 import tkinter as tk
-import tkinter.messagebox as tkm
+from  tkinter import messagebox as tkm
+import tkinter
+
+calc =  tk.Tk()
+calc.geometry("300x450")
+calc.title = ("電卓")
+
+entry = tk.Entry(justify = "right", width = 10, font = ("Times New Roman",40))
+entry.grid(row = 0, columnspan = 10)
 
 def button_click(event):
-    button = event.widget
-    num = button["text"]
-    entry.insert(tk.END, num)
-   
-def equal_click(event):
-    eqn = entry.get()
-    res = eval(eqn)
+    btn = event.widget
+    #button["bg"]="gray"
+    txt = btn["text"]
+    if txt=="−":
+        entry.insert(tk.END,"-")
+    elif txt=="×":
+        entry.insert(tk.END,"*")
+    else:
+        entry.insert(tk.END, txt)
+    #tkm.showinfo(txt, f"[{txt}]ボタンがクリックされました")
+
+def equal(event):
+    siki = entry.get()
     entry.delete(0, tk.END)
-    entry.insert(tk.END,str(res))
-
-def sqrt_click(event):
-    button = event.widget
-    eqn = entry.get()
-    res = math.sqrt(eval(eqn))
-    entry.delete(0, tk.END)
-    entry.insert(tk.END, res)
-
-
-def all_clear_click(event):
-    entry.delete(0, tk.END)
-
-def one_clear_click(event):
-    eqn = entry.get()
-    entry.delete(0, tk.END)
-    entry.insert(tk.END, eqn[:-1])
-
-root = tk.Tk()
-root.geometry("300x450")
-
-entry = tk.Entry(root, 
-                justify="right", 
-                width=10,
-                font=("Times New Roman", 40))
-entry.grid(row=0, column=0, columnspan=4)
-
-r , c = 1, 1
-for i,num in enumerate([7,8,9,4,5,6,1,2,3,"+",0], 1):
-    button = tk.Button(root, text=num, font=("Times New Roman", 20))
-    button.grid(row=r, column = c, padx=10, pady=0)
-    button.bind("<1>", button_click)
-    if i%3 == 0:
-        r += 1
-        c = 0
-    c += 1
+    kotae = eval(siki)
+    entry.insert(tk.END, kotae)
     
-button = tk.Button(root, 
-                  text="=", 
-                  font=("Times New Roman", 20)
-                  )
-button.grid(row=r, column = c, padx=10, pady=10)
-button.bind("<1>", equal_click)
+def delete(event):
+    entry.delete(0, tk.END)
+   
+fonts = ("Times New Roman", 20)
+fonts2=("Times New Roman", 10)
+x = 0
+list = [[7,1],[6,2],[6,1],[6,0],[5,2],[5,1],[5,0],[4,2],[4,1],[4,0]]
+for i in list:
+    button = tk.Button(text= x,font=fonts,width = 4, height = 1)
+    button.bind("<1>", button_click)
+    button.grid(row = i[0],column = i[1],padx=2,pady = 4)
+    x += 1
 
-button = tk.Button(root, 
-                  text="c", 
-                  font=("Times New Roman", 20)
-                  )
-button.grid(row=r + 1, column = c, padx=10, pady=10)
-button.bind("<1>", one_clear_click)
+    
+buttonequal = tk.Button(text='=',font=fonts,width = 4, height = 1,background = "gray")
+buttonequal.bind("<1>", equal)
+buttonequal.grid(row = 7,column = 4,padx=2,pady = 4)
 
-button = tk.Button(root, 
-                  text="ac", 
-                  font=("Times New Roman", 20)
-                  )
-button.grid(row=r + 1, column = c - 1, padx=10, pady=10)
-button.bind("<1>", all_clear_click)
+buttonDel = tk.Button(text='AC',font=fonts,width = 4, height = 1,background = "gray")
+buttonDel.bind("<1>", delete)
+buttonDel.grid(row = 2,column = 4,padx=2,pady = 4)
 
-button = tk.Button(root, 
-                  text="√", 
-                  font=("Times New Roman", 20)
-                  )
-button.grid(row=r + 1, column = c - 2, padx=10, pady=10)
-button.bind("<1>", sqrt_click)
 
-root.mainloop()
+list1 = [["+"],['−'],['×'],['/']]
+y = 3
+for i in list1:
+    button = tk.Button(text= i,font=fonts,width = 4, height = 1,background = "gray")
+    button.bind("<1>", button_click)
+    button.grid(row = y,column = 4,padx=2,pady = 4)
+    y += 1
+
+list1 = [['('],[')'],["C"]]
+y = 0
+for i in list1:
+    button = tk.Button(text= i,font=fonts,width = 4, height = 1,background = "gray")
+    if y==2:
+        button.bind("<1>", button_delete2)
+    else:
+        button.bind("<1>", button_click)
+    button.grid(row = 3,column = y,padx=2,pady = 4)
+    y += 1
+def button_delete2(event):
+    entry.delete(len(entry.get())-1,tk.END)
+calc.mainloop()
