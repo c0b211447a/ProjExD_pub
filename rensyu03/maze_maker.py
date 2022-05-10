68630638c7e579b85f7d517099390b4ead9e9214
c0b21070 <c0b21070c0@edu.teu.ac.jp>
2022-05-10 14:50:21 +0900

    5,6追加

--- rensyu03/maze_maker.py
+++ rensyu03/maze_maker.py
@@ -1,5 +1,13 @@
 import tkinter as tk
 
+def key_down(event):
+    global key
+    key = event.keysym
+
+def key_up(event):
+    global key
+    key =""
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -12,5 +20,7 @@ if __name__ == "__main__":
     canvas.create_image(cx, cy, image=tori, tag="tori")
     
     key = ""
-    
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
+
     root.mainloop()
