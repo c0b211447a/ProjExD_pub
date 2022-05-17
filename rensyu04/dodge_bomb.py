a7cbdc37001ba12282eb877da3dd6fcb1861b5f9
c0b21070 <c0b21070c0@edu.teu.ac.jp>
2022-05-17 17:31:25 +0900

    size

--- rensyu04/dodge_bomb.py
+++ rensyu04/dodge_bomb.py
@@ -1,4 +1,6 @@
 from re import X
+from this import d
+from turtle import width
 import pygame as pg
 import sys
 import random 
@@ -9,6 +11,7 @@ key_delta = {
              pg.K_LEFT : [-1,0],
              pg.K_RIGHT: [+1,0],
 }
+blocks = []
 
 def main():
     clock = pg.time.Clock()
@@ -21,22 +24,24 @@ def main():
     screen.blit(bg_img, bg_rect)               #背景画像用surfaceを画面用surfaceに貼り付ける            
 
     #練習３
+    size = 1
     tori_img = pg.image.load("fig/3.png")      #こうかとん画像用のsurface
-    tori_img = pg.transform.rotozoom(tori_img, 0, 2.0)
+    tori_img = pg.transform.rotozoom(tori_img, 0, size)
     tori_rect= tori_img.get_rect()             #こうかとん画像用のrect
     tori_rect.center = 900, 400                #こうかとん画像の中心座標を設定する
     screen.blit(tori_img, tori_rect)
 
     #練習５
+    
     bomb = pg.Surface((20,20))                            #爆弾用surface
     bomb.set_colorkey((0,0,0))                            #黒色部分を透過
     pg.draw.circle(bomb, (255, 0, 0), (10,10), 10)        #爆弾用surfaceに円を描く
     bomb_rect = bomb.get_rect()                           #爆弾用rect
-    bomb_rect.centerx = random.randint(0, sc_rect.width)
-    bomb_rect.centery = random.randint(0, sc_rect.height)
-    screen.blit(bomb, bomb_rect)
-    vx, vy = +1, +1                          #爆弾用surfaceを画面用surfaceに貼り付ける
-    
+    bomb_rect.centerx = random.randint(0, 1500)
+    bomb_rect.centery = random.randint(0, 800)
+    screen.blit(bomb, bomb_rect)                          #爆弾用surfaceを画面用surfaceに貼り付ける
+    vx, vy = +1, +1
+
     while True:
         #練習２
         screen.blit(bg_img, bg_rect)           #背景画像用surfaceを画面用surfaceに貼り付ける
@@ -55,7 +60,6 @@ def main():
         screen.blit(tori_img, tori_rect)
         
         #練習６
-        print(vx,vy)
         bomb_rect.move_ip(vx, vy)
         screen.blit(bomb, bomb_rect)
         #練習７
@@ -64,7 +68,17 @@ def main():
         vy *= y #縦方向に画面外なら、縦方向速度の符号反転
 
         #練習８
-        if tori_rect.colliderect(bomb_rect): return #こうかとん用rectが爆弾用rectと衝突していたらreturn
+        if tori_rect.colliderect(bomb_rect): #こうかとん用rectが爆弾用rectと衝突していたらreturn
+                size *= 1.1
+                tori_img = pg.image.load("fig/3.png")      #こうかとん画像用のsurface
+                tori_img = pg.transform.rotozoom(tori_img, 0, size)
+                tori_rect= tori_img.get_rect()             #こうかとん画像用のrect
+                tori_rect.centerx =  random.randint(0,sc_rect.width)
+                tori_rect.centery =  random.randint(0,sc_rect.height)               #こうかとん画像の中心座標を設定する
+                screen.blit(tori_img, tori_rect)
+                
+            
+            
 
         pg.display.update() #画面の更新
         clock.tick(1000)
